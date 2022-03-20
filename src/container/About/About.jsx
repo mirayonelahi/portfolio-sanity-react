e01fadbc2eda0394dff2c321ccb31f4e70e4bb4a
Ayon <mirelahi33@gmail.com>
2022-03-20 17:19:21 -0400

    favicon added

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -34,7 +34,9 @@ const About = () => {
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
+
       </div>
+
     </>
   );
 };
